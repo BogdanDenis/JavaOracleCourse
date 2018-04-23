63a56d305330f7a36c30bcfe91a126f74c4ba909
Denys Bohdan <Denys_Bohdan@epam.com>
2018-04-23 15:48:08 +0300

    Add Logo to Header

--- fe/src/components/header/header.jsx
+++ fe/src/components/header/header.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import { Logo } from '../../components';
+
 import './header.less';
 
 export const Header = ({ children }) => (
   <section className="header">
+    <Logo />
     {children}
   </section>
 );
--- fe/src/components/logo/logo.jsx
+++ fe/src/components/logo/logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './logo.less';
+
 export const Logo = () => (
   <section className="logo">
     <img
       src="images/logo/logo.svg"
       alt="IT"
-      className="logo"
+      className="icon"
     />
   </section>
 );
