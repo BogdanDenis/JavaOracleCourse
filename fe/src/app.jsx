8c19d214c8b2ba040e9d8b06b4a9b969aa8fa0eb
Denys Bohdan <Denys_Bohdan@epam.com>
2018-04-23 15:23:54 +0300

    Add login page component

--- fe/src/app.jsx
+++ fe/src/app.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import { Link, Route, Switch } from 'react-router-dom';
-import { withProps } from 'recompose';
+import { Route } from 'react-router-dom';
 
 import * as pages from './pages';
 import * as routes from './constants';
@@ -12,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Route exact path={routes.START_ROUTE} component={pages.Login} /> */}
+        <Route exact path={routes.LOGIN_ROUTE} component={pages.Login} />
       </div>
     );
   }
---
+++ fe/src/pages/login/login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  render() {
+    return (
+      <section className="login-page">
+
+      </section>
+    );
+  }
+}
