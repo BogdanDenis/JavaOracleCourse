8549ee9d9aef143e73269f14a08db1d00a560cc7
BogdanDenis <denys.bohdan@nure.ua>
2018-05-01 19:19:33 +0300

    add header styles

--- fe/src/components/menu-list/components/menu-list-item/menu-list-item.jsx
+++ fe/src/components/menu-list/components/menu-list-item/menu-list-item.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import './menu-list-item.sass';
+
 const MenuListItem = ({ label, url }) => (
   <Link className="menu-list__item" to={url}>
     <p className="menu-list__item__title">{label}</p>
