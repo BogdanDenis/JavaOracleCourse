e8f3c4eac353e92b3343da629e0ee69166d0d552
BogdanDenis <denys.bohdan@nure.ua>
2018-05-02 14:59:58 +0300

    add CORS for requests from localhost:3000

--- java/src/main/java/issuetracker/Application.java
+++ java/src/main/java/issuetracker/Application.java
@@ -2,6 +2,23 @@ package issuetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+class WebConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedHeaders("authorization", "content-type")
+				.allowedMethods("HEAD", "GET", "POST", "PATCH");
+	}
+}
 
 @SpringBootApplication
 public class Application {
