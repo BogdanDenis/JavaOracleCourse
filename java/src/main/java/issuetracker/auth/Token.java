58a3ed019c37bce26e948ecfe04d5d8da566eb4a
BogdanDenis <denys.bohdan@nure.ua>
2018-05-23 23:52:31 +0300

    add auth to java

---
+++ java/src/main/java/issuetracker/auth/Token.java
@@ -0,0 +1,29 @@
+package issuetracker.auth;
+
+public class Token {
+    private String access_token;
+    private String type;
+
+    public Token() {}
+
+    public Token(String access_token, String type) {
+        this.access_token = access_token;
+        this.type = type;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
